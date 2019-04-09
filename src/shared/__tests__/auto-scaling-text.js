5b83bf93cdb4c798210cfba02da1e5e3238c8835
Chris Chuang <chrischuang0421@gmail.com>
2019-04-09 14:54:13 +0800

    First snapshot try

--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -4,6 +4,7 @@ import {render} from 'react-testing-library'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-  const {container} = render(<AutoScalingText />)
-  console.log(container.innerHTML)
+  // const {container} = render(<AutoScalingText />)
+  // console.log(container.innerHTML)
+  render(<AutoScalingText />)
 })
