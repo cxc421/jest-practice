c1baea8660c3d0aecfc13a01c3d272d236d9b364
Chris Chuang <chrischuang0421@gmail.com>
2019-04-11 13:33:01 +0800

    Partial work of coverage

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,19 @@ module.exports = {
     '\\.css$': cssMockPath,
   },
   setupFilesAfterEnv: [require.resolve('./test/setup-test.js')],
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 17,
+      branches: 4,
+      lines: 17,
+      functions: 20,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      lines: 100,
+      functions: 100,
+    },
+  },
 }
